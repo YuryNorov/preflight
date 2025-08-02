efac2f7f920cbde2273fca6026e983f5b67b62b6
Yury Norov <yury.norov@gmail.com>
2025-08-01 21:27:57 -0400

    Revert "analytical"
    This reverts commit 5e5f068862739adf121c9de43917a0527663909c.

--- preflight.py
+++ preflight.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from scipy import interpolate
+import numpy as np
+
 import sys
 import argparse
 import math
@@ -34,6 +37,18 @@ def weight_balance(front, middle, rear, fuel_main, fuel_tips, fbaggage, rbaggage
             )
 
     return weight, moment, moment / weight
+
+wind = np.array([ 0, 5, 10, 15])
+to50 = np.array([ 980, 1170, 1390, 1650, 1920, 2280, 2950, 3810 ])
+dist = np.array([[ 980,  870,  770,  670  ],
+                 [ 1170, 1050, 940,  820  ],
+                 [ 1390, 1240, 1110, 980  ],
+                 [ 1650, 1490, 1340, 1190 ],
+                 [ 1920, 1750, 1590, 1450 ],
+                 [ 2280, 2060, 1860, 1690 ],
+                 [ 2950, 2650, 2420, 2200 ],
+                 [ 3810, 3480, 3190, 2950 ],
+               ])
 
 # https://ourairports.com/data/airports.csv
 def load_airports_csv(filepath="airports.csv"):
@@ -131,20 +146,79 @@ def density_altitude(elev, altimeter, oat):
 
     return int(pa + (oat - tstd) * 120)
 
-def to50(w, da):
-    return 1019.5354 -1.908517e-01 * da +3.105556e-01 * w +1.228355e-05 * da**2 +4.500000e-05 * da * w -4.861111e-05 * w**2
+############################
+###  Take-off Distance  ####
+############################
 
-def la_50(w, da):
-    return 1234.1616 +3.393290e-02 * da +8.023611e-02 * w +8.000541e-07 * da**2 +3.395833e-06 * da*w -6.770833e-06 * w*2
+def takeoff_50_nowind(weight, da) : 
+    DA = np.arange(0, 9000, 1000)  # 9 values: 0 to 8000
+    W = np.array([4000, 4400, 4800, 5200])  # 4 values
+    DIST = np.array([[1380, 1440, 1220, 1290, 1390, 1490, 1600, 1700, 1820],
+                     [1250, 1300, 1370, 1470, 1570, 1680, 1800, 1920, 2050],
+                     [1400, 1450, 1540, 1650, 1800, 1950, 2100, 2250, 2400],
+                     [1580, 1630, 1730, 1860, 2020, 2200, 2400, 2600, 2800]])
 
-def hw_takeoff(w, to):
-    ret =  43.6873 + 9.639547e-01 * to - 1.336320e+01 * w + 5.836497e-06 * to**2 - 1.337426e-02 * to*w + 3.625000e-01 * w**2
-    print(type(ret))
-    print(ret)
-    return ret
+    # Interpolator
+    interp_func = interpolate.RegularGridInterpolator((W, DA), DIST, bounds_error=False, fill_value=None)
+    return int(interp_func(np.array([weight, da]))[0])
 
-def hw_land(w, la):
-    return 41.7542 +9.479427e-01 * la - 6.375855e+00 * w + 1.576817e-05 * la**2 - 8.293356e-03 * w*la + 2.727273e-02 * w**2
+def landing_50_nowind(weight, da) : 
+    DA = np.arange(0, 9000, 1000)  # 9 values: 0 to 8000
+    W = np.array([4000, 4400, 4800, 5200])  # 4 values
+    DIST = np.array([[1380, 1407, 1437, 1470, 1505, 1540, 1575, 1615, 1655],
+                     [1480, 1510, 1540, 1575, 1610, 1650, 1690, 1730, 1775],
+                     [1580, 1610, 1645, 1685, 1725, 1770, 1810, 1855, 1900],
+                     [1680, 1715, 1750, 1790, 1835, 1880, 1925, 1975, 2025]])
+
+    # Interpolator
+    interp_func = interpolate.RegularGridInterpolator((W, DA), DIST, bounds_error=False, fill_value=None)
+    return int(interp_func(np.array([weight, da]))[0])
+
+def headwind_takeoff(wind_mph, to50_ft) : 
+    DIST = np.array([[ 980,  870,  770,  670  ],  
+                     [1170, 1050,  940,  820  ],  
+                     [1390, 1240, 1110,  980  ],  
+                     [1650, 1490, 1340, 1190 ],
+                     [1920, 1750, 1590, 1450 ],
+                     [2280, 2060, 1860, 1690 ],
+                     [2950, 2650, 2420, 2200 ],
+                     [3810, 3480, 3190, 2950 ]])
+    
+    # Grid axes
+    wind = np.array([0, 5, 10, 15])  # x-axis
+    to50 = np.array([980, 1170, 1390, 1650, 1920, 2280, 2950, 3810])  # y-axis
+    
+    # Interpolator with extrapolation allowed
+    interp_func = interpolate.RegularGridInterpolator((to50, wind), DIST,
+                                                      method="linear",
+                                                      bounds_error=False, fill_value=None)
+    return int(interp_func(np.array([to50_ft, wind_mph]))[0])
+
+def headwind_land(wind_mph, land50_ft):
+    DIST = np.array([
+                 [ 1190, 1105, 1025,  950 ],  #  1  
+                 [ 1270, 1190, 1105, 1020 ],  #  2
+                 [ 1350, 1265, 1180, 1100 ],  #  3
+                 [ 1430, 1345, 1255, 1170 ],  #  4
+                 [ 1505, 1415, 1330, 1240 ],  #  5
+                 [ 1600, 1500, 1400, 1300 ],  #  6
+                 [ 1660, 1560, 1460, 1360 ],  #  7
+                 [ 1770, 1660, 1550, 1450 ],  #  8
+                 [ 1870, 1755, 1645, 1540 ],  #  9
+                 [ 1990, 1880, 1770, 1660 ],  # 10
+                 [ 2110, 1995, 1880, 1765 ]  # 11
+             ])
+    # Grid axes
+    wind = np.array([0, 5, 10, 15])  # x-axis
+    la50 = np.array([1190, 1270, 1350, 1430,
+                     1505, 1600, 1660, 1770,
+                     1870, 1990, 2110])
+    
+    # Interpolator with extrapolation allowed
+    interp_func = interpolate.RegularGridInterpolator((la50, wind), DIST,
+                                                      method="linear",
+                                                      bounds_error=False, fill_value=None)
+    return int(interp_func(np.array([land50_ft, wind_mph]))[0])
 
 if __name__ == "__main__":
 
@@ -182,24 +256,18 @@ if __name__ == "__main__":
 
     da = density_altitude(elevation, d['pressure_inhg'], d['temperature_c'])
     pa = pressure_altitude(elevation, d['pressure_inhg'])
-    #to_nw = takeoff_50_nowind(weight, da)
-    to_nw = to50(weight, da)
-    #land_nw = landing_50_nowind(weight, da)
-    land_nw = la_50(weight, da)
+    to_nw = takeoff_50_nowind(weight, da)
+    land_nw = landing_50_nowind(weight, da)
 
     headwind  = int(d['wind_speed_kt'] * math.cos(math.radians(d['wind_direction'] - runway*10)))
     crosswind = int(d['wind_speed_kt'] * math.sin(math.radians(d['wind_direction'] - runway*10)))
-    #to = headwind_takeoff(headwind * 1.15, to_nw) 
-    to = hw_takeoff(headwind * 1.15, to_nw) 
-    #land = headwind_land(headwind * 1.15, land_nw)
-    land = hw_land(headwind * 1.15, land_nw)
+    to = headwind_takeoff(headwind * 1.15, to_nw) 
+    land = headwind_land(headwind * 1.15, land_nw)
 
     headwind_gust  = int(d['wind_gust_kt'] * math.cos(math.radians(d['wind_direction'] - runway*10)))
     crosswind_gust = int(d['wind_gust_kt'] * math.sin(math.radians(d['wind_direction'] - runway*10)))
-    #to_gust = headwind_takeoff(headwind_gust * 1.15, to_nw) 
-    to_gust = hw_takeoff(headwind_gust * 1.15, to_nw) 
-    #land_gust = headwind_land(headwind_gust * 1.15, land_nw) 
-    land_gust = hw_land(headwind_gust * 1.15, land_nw) 
+    to_gust = headwind_takeoff(headwind_gust * 1.15, to_nw) 
+    land_gust = headwind_land(headwind_gust * 1.15, land_nw) 
 
     print(f"{'\nWeather':21} {'Unit':10} {'Value'}")
     print(f"{'-'*20} {'-'*10} {'-'*8}")
